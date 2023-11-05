f48b96a295dc060f57ddf59e662f192dc192eec8
Mohamed Nagy <mohmmednagy@gmail.com>
2023-11-05 10:30:58 +0200

    Add Task Two

---
+++ Assignments/02_Embedded_C/LAB2/uart.c
@@ -0,0 +1,11 @@
+#define UART0DR   *((volatile unsigned int* const)(0x101f1000))
+
+void Uart_Send_String(unsigned char *P_tx_string)
+{
+
+    while(*P_tx_string)
+    {
+        UART0DR = (unsigned int)(*P_tx_string);
+        P_tx_string++;
+    }
+}
---
+++ Assignments/02_Embedded_C/LAB2/uart.h
@@ -0,0 +1,6 @@
+#ifndef _UART_H_
+#define _UART_H_
+
+extern void Uart_Send_String(unsigned char *P_tx_string);
+
+#endif // UART_H_
