d4a409e199ea124ea2b2a4cde4c3536f28926434
Mohamed Nagy <mohmmednagy@gmail.com>
2024-01-30 11:00:44 +0200

    add AFIO driver to configure EXTI lines

---
+++ STM32F103C9T6_Drivers/02_MCAL/03_AFIO/AFIO_config.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   AFIO_CONFIG.h
+ * Author: Mohamed_Nagy
+ * https://github.com/Ged0oo 
+ * https://www.linkedin.com/in/mohamednagyofficial/
+ * Created on January 29, 2024, 8:41 PM
+ */
+
+
+
+#ifndef __AFIO_CONFIG_H__
+#define __AFIO_CONFIG_H__
+
+#endif  __AFIO_CONFIG_H__
