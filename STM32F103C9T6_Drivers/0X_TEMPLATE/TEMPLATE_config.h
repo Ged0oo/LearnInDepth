c1bedd0c7fb177408f8ccbacfa0ec8d3b78908a7
Mohamed Nagy <mohmmednagy@gmail.com>
2024-01-30 08:38:47 +0200

    Add stm32 drivers templete

---
+++ STM32F103C9T6_Drivers/0X_TEMPLATE/TEMPLATE_config.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   TEMPLATE_CONFIG.h
+ * Author: Mohamed_Nagy
+ * https://github.com/Ged0oo 
+ * https://www.linkedin.com/in/mohamednagyofficial/
+ * Created on January 29, 2024, 8:41 PM
+ */
+
+
+
+#ifndef __TEMPLATE_CONFIG_H__
+#define __TEMPLATE_CONFIG_H__
+
+#endif  __TEMPLATE_CONFIG_H__
---
+++ STM32F103C9T6_Drivers/0X_TEMPLATE/TEMPLATE_program.c
@@ -0,0 +1,14 @@
+/* 
+ * File:   TEMPLATE_PROGRAM.c
+ * Author: Mohamed_Nagy
+ * https://github.com/Ged0oo 
+ * https://www.linkedin.com/in/mohamednagyofficial/
+ * Created on January 29, 2024, 8:41 PM
+ */
+
+
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
+#include "TEMPLATE_interface.h"
+#include "TEMPLATE_private.h"
+#include "TEMPLATE_config.h"
