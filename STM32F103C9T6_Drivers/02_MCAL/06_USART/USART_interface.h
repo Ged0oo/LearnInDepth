d69df64ba348734caac4a718c4a520e28dc32cb5
Mohamed Nagy <mohmmednagy@gmail.com>
2024-02-05 14:59:33 +0200

    add templete files for USART driver

---
+++ STM32F103C9T6_Drivers/02_MCAL/06_USART/USART_interface.h
@@ -0,0 +1,13 @@
+/* 
+ * File:   USART_INTERFACE.h
+ * Author: Mohamed_Nagy
+ * https://github.com/Ged0oo 
+ * https://www.linkedin.com/in/mohamednagyofficial/
+ * Created on February 5, 2024, 8:41 PM
+ */
+
+
+#ifndef __USART_INTERFACE_H__
+#define __USART_INTERFACE_H__
+				   
+#endif  __USART_INTERFACE_H__
